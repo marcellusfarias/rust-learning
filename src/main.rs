f4dd9dc04650d2d1792611d4caf7c24d11ec53cc
unknown <mafarias@concordservicing.com>
2021-08-20 14:28:10 -0300

    Deserializing products.json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use serde_json::{Map, Value};
+use std::fs;
+
+use crate::data::Product;
+mod data;
+
 fn main() {
     println!("Hello, world!");
+
+    let contents = fs::read_to_string("./data/products.json")
+        .expect("Something went wrong reading the file");
+
+    match serde_json::from_str::<Vec<Product>>(contents.as_str())  {
+        Err(e) => {
+            println!("Unable to convert JSON to Struct. Message received from topic is: {:#?}. Error is {:?}", contents.as_str(), e);
+        }
+        Ok(vector) => {
+            for x in vector {
+                println!("{:?}", &x);
+            }
+        }
+    }
 }
