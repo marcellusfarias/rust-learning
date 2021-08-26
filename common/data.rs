dd04e369d984dc3133d91b7c89426d79270622b5
Marcellus Farias <marcelluscfarias@gmail.com>
2021-08-26 14:32:51 -0300

    Billing more than one product!

--- common/data.rs
+++ common/data.rs
@@ -2,32 +2,41 @@ use serde_json::{Map};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashMap;
+use std::fs;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Billing {
     pub client_name: String,
-    pub products_ordered: HashMap<String, Product>, //product and quantity
+    pub products: Vec<Product>,
 }
 
 impl Billing {
     pub fn serialize(&self) -> serde_json::Value {
         json!({
             "client_name": self.client_name,
-            "products_ordered": self.products_ordered
+            "products": self.products
         })
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Product {
     pub name: String,
     pub price: f32,
+}
+
+pub fn get_list_products() -> Vec<Product> {
+    serde_json::from_str::<Vec<Product>>(fs::read_to_string("./data/products.json").unwrap().as_str()).unwrap()
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MakeOrderRequest {
     pub client_name: String,
     pub product_name: String
+}
+
+pub fn get_make_order_request(request: &str) -> MakeOrderRequest {
+    serde_json::from_str::<MakeOrderRequest>(request).unwrap()
 }
 
 impl MakeOrderRequest {
--- server/src/server.rs
+++ server/src/server.rs
@@ -2,74 +2,93 @@ use actix_web::{web, HttpResponse, Responder};
 use std::fs;
 use textcode::iso8859_1;
 
-use std::collections::HashMap;
-
 #[path = "../../common/data.rs"]
 mod data;
-use data::{Product, Billing, MakeOrderRequest};
+use data::{Billing, MakeOrderRequest, Product};
 
-pub const SERVER_ADDRESS: &str = "127.0.0.1:8080"; 
+pub const SERVER_ADDRESS: &str = "127.0.0.1:8080";
 
 pub async fn list_products() -> impl Responder {
-    let list_products = serde_json::from_str::<Vec<Product>>(fs::read_to_string("./data/products.json").unwrap().as_str()).unwrap();
+    let list_products = data::get_list_products();
     HttpResponse::Ok().json(list_products)
 }
 
 pub async fn make_order(raw_request: web::Bytes) -> impl Responder {
-    let raw_req = iso8859_1::decode_to_string(&raw_request.to_vec());    
-    let request: MakeOrderRequest = match serde_json::from_str(raw_req.as_str()) {
-        Ok(val) => val,
-        Err(_) => {
-            println!("make_order: error 1");
-            println!("request: {}", raw_req.as_str());
-            return HttpResponse::ExpectationFailed()
-                .json("Service was unable to deserialize the request")
-        }
-    };
-    
-    let mut list_products: Vec<Product> = serde_json::from_str::<Vec<Product>>(fs::read_to_string("./data/products.json").unwrap().as_str()).unwrap();
-    
-    if !list_products.iter().any(|i: &Product| i.name == request.product_name) {
-        println!("make_order: error 2");
+    let raw_req = iso8859_1::decode_to_string(&raw_request.to_vec());
+    let request: MakeOrderRequest = data::get_make_order_request(raw_req.as_str());
+    let list_products: Vec<Product> = data::get_list_products();
+
+    if !list_products
+        .iter()
+        .any(|i: &Product| i.name == request.product_name)
+    {
         return HttpResponse::ExpectationFailed()
-            .json(["Product ", request.product_name.as_str(), " doesnt exist"].join(""))
+            .json(["Product ", request.product_name.as_str(), " doesnt exist"].join(""));
     }
-    
-    println!("Updating order for the client {} with product {}", request.client_name, request.product_name);
+
+    println!(
+        "Updating order for the client {} with product {}",
+        request.client_name, request.product_name
+    );
 
     let mut list_billings: Vec<Billing> = Vec::<Billing>::new();
 
     match fs::read_to_string("./data/billings.json") {
-        Err(e) => {
-
-        }, 
+        Err(e) => {}
         Ok(billings) => {
             if billings.trim().len() > 0 {
-                list_billings = serde_json::from_str(billings.as_str()).unwrap();
+                list_billings = serde_json::from_str::<Vec<Billing>>(billings.as_str()).unwrap();
             }
         }
     }
 
-    if  list_billings.is_empty() || !list_billings.iter().any(|i: &Billing| i.client_name == request.client_name) {
-        let product_index =  list_products.iter().position(|r: &Product| r.name == request.product_name).unwrap();
-        let mut map: HashMap<String, Product> = HashMap::<String, Product>::new();
-        let product: &Product = &list_products[product_index];
-        
-        map.insert("1".to_string(), Product {name: product.name.as_str().to_string(), price: product.price});
-        // map.insert(vector, 1);
+    let product_index = list_products
+        .iter()
+        .position(|r: &Product| r.name == request.product_name)
+        .unwrap();
+    let product: &Product = &list_products[product_index];
 
-        let new_bill: Billing = Billing {
+    if list_billings.is_empty()
+        || !list_billings
+            .iter()
+            .any(|i: &Billing| i.client_name == request.client_name)
+    {
+        let mut products: Vec<Product> = Vec::<Product>::new();
+        products.push(product.clone());
+
+        list_billings.push(Billing {
             client_name: request.client_name,
-            products_ordered: map
-        };
+            products: products,
+        });
 
-        println!("Creating billing.json...");
+        fs::write(
+            "./data/billings.json",
+            serde_json::json!(list_billings).to_string(),
+        );
+    } else {
+        let mut bill = list_billings
+            .iter()
+            .filter(|&x| *x.client_name == request.client_name)
+            .nth(0)
+            .unwrap()
+            .clone();
 
-        fs::write("./data/billings.json", new_bill.serialize().to_string());
+        bill.products.push(product.clone());
+
+        list_billings = list_billings
+            .iter()
+            .filter(|&x| *x.client_name != request.client_name)
+            .cloned()
+            .collect();
+
+        list_billings.push(bill);
+
+        fs::write(
+            "./data/billings.json",
+            serde_json::json!(list_billings).to_string(),
+        );
     }
-    else{
-        println!("Not creating billing.json...");
-    }
-
+    
+    println!("Bill updated!");
     HttpResponse::Ok().body("200")
 }
